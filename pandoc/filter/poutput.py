48fc957640a88cd38dffc35152985e8e8aba15a6
csk <conradsteink@gmail.com>
2017-07-31 23:53:51 -0400

    Add run_time, elapsed time and status per plugin to pandoc poutput filter

--- pandoc/filter/poutput.py
+++ pandoc/filter/poutput.py
@@ -4,11 +4,23 @@
 Plugin Output Filter
 """
 
-
+import sys
 import requests
 import json
+from slugify import slugify
 
-from pandocfilters import toJSONFilter, CodeBlock, Para, Image, get_caption, get_extension, get_value
+from pandocfilters import toJSONFilter, Header, CodeBlock, Space, Para, Str, Strong, Emph, Image, get_caption, get_extension, get_value
+
+
+def text2pandoc(str):
+    result = []
+    arr = str.split(" ")
+    if len(arr) > 0:
+        result.append(Str(arr[0]))
+    for x in arr[1:]:
+        result.append(Space)
+        result.append(Str(x))
+    return result
 
 
 def poutput(key, value, format, _):
@@ -16,12 +28,15 @@ def poutput(key, value, format, _):
         [[ident, classes, keyvals], code] = value
 
         if "poutput" in classes:
-            response = requests.get('http://192.168.0.10:8009/api/targets/1/poutput/names/')
+            _type, keyvals = get_value(keyvals, u"type", u"index")
+
+            response = requests.get('http://127.0.0.1:8009/api/targets/1/poutput/names/')
             content = response.content
             content = content.strip('\n') # Weird newline chars causing issues
             output = json.loads(content)
 
-            tbl = """
+            if _type == u"index":
+                tbl = """
 ---
 caption: 'Tests Ejecutados'
 alignment: C
@@ -30,10 +45,36 @@ markdown: True
 ---
 Tests Ejecutados
 """
-            tests = "\n".join(output.keys())
+                tests = "\n".join(output.keys())
 
-            tbl += tests
-            return CodeBlock([ident, ['table'], keyvals], tbl)
-
+                tbl += tests
+                return [CodeBlock([ident, ['table'], keyvals], tbl)]
+            elif _type == u"all_detail":
+                sys.stderr.write('cvss using type\'' + _type + '\'\n')
+                tbl_detail = """
+---
+caption: 'Tests Ejecutados'
+alignment: C
+table-width: 2/3
+markdown: True
+---
+Tests Ejecutados
+"""
+                result = []
+                for k,v in output.iteritems():
+                    content = k
+                    result.append(Header(2, [slugify(content), [], []], text2pandoc(content)))
+                    for item in v['data']:
+                        tests = ",".join(['RUNTIME', 'TIME INTERVAL', 'STATUS'])
+                        tests += "\n"+",".join([item['run_time'].replace(","," "), "%s - %s" % (item['start_time'],item['end_time']), item['status']])
+                        result.append(CodeBlock([ident, ['table'], keyvals], tbl_detail + tests))
+                        response = requests.get('http://127.0.0.1:8009/api/targets/1/poutput/?plugin_code=%s' % (k,))
+                        content = response.content
+                        content = content.strip('\n') # Weird newline chars causing issues
+                        output = json.loads(content)
+                return result
+            else:
+                raise NotImplementedError()
+            
 if __name__ == "__main__":
     toJSONFilter(poutput)
